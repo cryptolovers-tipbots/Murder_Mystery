2aeaed3e2dc8fc7308d6b2f232b055c822fb51a1
CryptoLover705 <50221455+CryptoLover705@users.noreply.github.com>
2020-09-16 16:50:02 +0100

    Create MurderMystery.py

---
+++ MurderMystery.py
@@ -0,0 +1,46 @@
+#.       MURDER MYSTERY🔪.       #   
+#._______________________________#
+
+#you are trapped in a room with 10 people one of them is a murderer he will kill u soon
+#your goal is to catch the murderer if you do so you win else you DIE⚰️💀
+
+#here is a list of people in the room guess who is the murderer and type their name to catch them
+
+"""
+1)clark
+2)frank
+3)jim
+4)michel
+5)jeff
+6)charlie
+7)harvey
+8)tony
+9)lofaa
+10)mark
+"""
+#good luck👍
+#murderer is random always
+
+
+
+
+
+
+import random
+#var
+choosen=input().lower()
+#list
+murderer=["jim","lofaa","mark","tony","harvey","charlie","jeff","michel","frank","clark"]
+choosenone=random.choice(murderer)
+#game
+if choosen==choosenone:print("you caught the murderer\n you won🏆")
+else:print("you failed to catch the murderer\nhe killed you💀\n murderer was "+choosenone+"🔪\ngame over")
+
+
+
+#info
+"""
+by:CryptoLover705
+date:16sep 2020
+"""
+ 
